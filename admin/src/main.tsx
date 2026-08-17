468afb7f4cef5644c4cfcd9a970b042a0287cbf9
Abdul Ahad Butt <abdul.ahad.butt@example.com>
2026-08-17 11:24:32 -0700

    Fix index.css import in admin panel

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '../shared/styles/index.css'
+import "../../shared/styles/index.css";
 import AdminLayout from './components/AdminLayout'
 
 createRoot(document.getElementById('root')!).render(
